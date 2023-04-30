7b97edc950422ae32ffc5d25acce89086bf6390e
Andrii Livinskyi <landry2022@ukr.net>
2023-04-30 21:12:33 +0300

    Убрана функция getUsers, удалил закоментированные строки.

--- src/componets/Sidebar/Pages/Users/UsersClassComponent.jsx
+++ src/componets/Sidebar/Pages/Users/UsersClassComponent.jsx
@@ -12,47 +12,29 @@ class Users extends React.Component {
   // наш контсруктор вызывается только один раз при создании объекта
   // поэтому в конструкторе и загружаем список пользователей
   // это происходит один раз и не требует проверки на нулевую длину
-  // и не нужна пнопка загрузки пользователей
+  // и не нужна кнопка загрузки пользователей
+  // при повторной отрисовке компоненты используется уже созданый объект
 
+  // если конструктор не зоздает свойств для этой компоненты
+  // то его можно не писать
+  // в нашем случае нужно писать т.к. есть axios
   constructor(props) {
     super(props);
 
-    // alert("создан новый объект");
-
-    // if (this.props.users.length === 0) {
     axios
       .get(" https://643e90e66c30feced82c8d63.mockapi.io/seria/0/bases")
       .then((response) => {
+        // props становится свойством объекта, поэтому нужно писать this.props
         this.props.setUsers(response.data);
       });
-
-    // axios
-    //   .get(" https://social-network.samuraijs.com/api/1.0/users")
-    //   .then((response) => {
-    //     props.setUsers(response.data.items);
-    //   });
-    // }
   }
 
-  getUsers = () => {
-    // if (this.props.users.length === 0) {
-    //   axios
-    //     .get(" https://643e90e66c30feced82c8d63.mockapi.io/seria/0/bases")
-    //     .then((response) => {
-    //       this.props.setUsers(response.data);
-    //     });
-    //   // axios
-    //   //   .get(" https://social-network.samuraijs.com/api/1.0/users")
-    //   //   .then((response) => {
-    //   //     props.setUsers(response.data.items);
-    //   //   });
-    // }
-  };
-
+  // props в рендер не приходят
+  // render обязательный метод классовой компоненты
+  // он возвращает jsx разметку.
   render = () => {
     return (
       <div>
-        {/* <button onClick={this.getUsers}>Get users</button> */}
         <div>
           {this.props.users.map((u) => (
             // всегда нужен  key={u.id}
@@ -98,8 +80,6 @@ class Users extends React.Component {
                   <div>{u.status} </div>
                 </span>
                 <span>
-                  {/* <div>{"u.location.country"}</div>
-                    <div>{"u.location.city"}</div> */}
                   <div>{u.country}</div>
                   <div>{u.city}</div>
                 </span>
@@ -111,85 +91,5 @@ class Users extends React.Component {
     );
   };
 }
-
-// const Users = (props) => {
-
-//   let getUsers = () => {
-//     if (props.users.length === 0) {
-//       axios
-//         .get(" https://643e90e66c30feced82c8d63.mockapi.io/seria/0/bases")
-//         .then((response) => {
-//           props.setUsers(response.data);
-//         });
-
-//       // axios
-//       //   .get(" https://social-network.samuraijs.com/api/1.0/users")
-//       //   .then((response) => {
-//       //     props.setUsers(response.data.items);
-//       //   });
-//     }
-//   };
-
-//   return (<div>
-
-//     <button onClick={getUsers} >Get users</button>
-
-//       <div>
-//         {props.users.map((u) => (
-//           // всегда нужен  key={u.id}
-//           <div key={u.id}>
-//             <span>
-//               <div>
-//                 <img
-//                   className={style.photo}
-//                   src={
-//                     u.photosSmall != null
-//                       ? u.photosSmall
-//                       : // : "https://klike.net/uploads/posts/2019-03/1551511827_30.jpg"
-//                         // или
-//                         userFotoBlank
-//                   }
-//                   alt=""
-//                 />
-//               </div>
-//               {/* выводим на кнопку нужное состояние */}
-//               <div>
-//                 {u.followed ? (
-//                   <button
-//                     onClick={() => {
-//                       props.unfollow(u.id);
-//                     }}
-//                   >
-//                     Unfollow
-//                   </button>
-//                 ) : (
-//                   <button
-//                     onClick={() => {
-//                       props.follow(u.id);
-//                     }}
-//                   >
-//                     Follow
-//                   </button>
-//                 )}
-//               </div>
-//             </span>
-//             <span>
-//               <span>
-//                 <div>{u.name}</div>
-//                 <div>{u.status} </div>
-//               </span>
-//               <span>
-//                 {/* <div>{"u.location.country"}</div>
-//                 <div>{"u.location.city"}</div> */}
-//                 <div>{u.country}</div>
-//                 <div>{u.city}</div>
-//               </span>
-//             </span>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
 
 export default Users;
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,70 +4,23 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET-USERS";
 // создается как бы шаблон, достаточно одного элемента
 let initialState = {
-  
   users: [
-    // {
-    //   id: 0,
-    //   fullName: "Andrii",
-    //   status: "I am a teacher.",
-    //   location: {
-    //     country: "Ukraine",
-    //     city: "Kyiv",
-    //   },
-    //   followed: true,
-    //   photoUrl:
-    //     "https://bipbap.ru/wp-content/uploads/2021/07/ae785830348d0d06a40e79f2219052ad-730x548.jpg",
-    // },
-    // {
-    //   id: 1,
-    //   fullName: "Victipiy",
-    //   status: "I am a bookceaper.",
-    //   location: {
-    //     country: "Ukraine",
-    //     city: "Odessa",
-    //   },
-    //   followed: false,
-    //   photoUrl:
-    //     "https://klike.net/uploads/posts/2019-03/medium/1551511819_33.jpg",
-    // },
-    // {
-    //   id: 2,
-    //   fullName: "Petro",
-    //   status: "I no now.",
-    //   location: {
-    //     countre: "Turque",
-    //     city: "Atambul",
-    //   },
-    //   followed: true,
-    //   photoUrl: "https://klike.net/uploads/posts/2019-03/1551511851_21.jpg",
-    // },
-    // {
-    //   id: 3,
-    //   fullName: "Helen",
-    //   status: "I am a girl.",
-    //   location: {
-    //     country: "Germany",
-    //     city: "Drezden",
-    //   },
-    //   followed: false,
-    //   photoUrl: "https://klike.net/uploads/posts/2019-03/1551511827_30.jpg",
-    // },
+    // пустой массив для париема пользователей
   ],
 };
 
 const usersReduser = (state = initialState, action) => {
-
-   switch (action.type) {
+  switch (action.type) {
     case FOLLOW:
-       return {
+      return {
         // возвращаем копию стэйта
-         ...state,
-         
+        ...state,
+
         //  возвращаем копию пользователей
         // users: { ...state.users },
-         
+
         //  или тоже самое, но через MAP
-        // map возвращает копию массива на основе заданного 
+        // map возвращает копию массива на основе заданного
         // при этом происходит перебор по условиям
         users: state.users.map(
           // вызываем безимянную функцию
@@ -105,6 +58,8 @@ const usersReduser = (state = initialState, action) => {
         // берем существующий стайт и перетираем прользователей
         // склейкой старых и пришедших
         // users: [...state.users, ...action.users],
+
+        // уже этого не делаем
         users: [...action.users],
       };
     // break;
@@ -119,7 +74,6 @@ export const unfollowACreator = (userId) => ({ type: UNFOLLOW, userId });
 
 // заполняем пользователей
 export const setUsersACreator = (users) => {
-
   return { type: SET_USERS, users };
 };
 
