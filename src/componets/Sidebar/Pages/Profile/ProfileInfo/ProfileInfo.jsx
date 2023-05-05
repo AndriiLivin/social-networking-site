f48e3775b128ce664229ec9daa0987fc9fd4aa13
Andrii Livinskyi <landry2022@ukr.net>
2023-05-05 20:36:40 +0300

    Профидь раскрасил стилями.

--- src/componets/Sidebar/Pages/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/componets/Sidebar/Pages/Profile/ProfileInfo/ProfileInfo.jsx
@@ -16,10 +16,32 @@ const ProfileInfo = (props) => {
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0HtBAPtxyscUfU26bE5hXknFbOm2I6d-rw&usqp=CAU"
         ></img>
       </div>
-      <div className={style.ava}>
-        <img src={props.profile.photosLarge} alt="" width="300px" />
-        AVATA + Description
-      </div>
+      <section className={style.description}>
+        <div className={style.ava}>
+          <img src={props.profile.avatar} alt="" width="100px" />
+          <div className="">Моя аватарка</div>
+        </div>
+
+        <div className={style.text}>
+          <div className={style.title_text}>Страна:</div>
+          <div>{props.profile.country}</div>
+          <br />
+          <div className={style.title_text}>Город:</div>
+          <div>{props.profile.city}</div>
+        </div>
+        <div className={style.text}>
+          <div className={style.title_text}>Меня зовут:</div>
+          <div>{props.profile.name}</div>
+          <br />
+          <div className={style.title_text}>Статус:</div>
+          <div>{props.profile.status}</div>
+        </div>
+
+        <div className={style.foto}>
+          <img src={props.profile.photosLarge} alt="" width="300px" />
+          <div className="">Мое удачное фото</div>
+        </div>
+      </section>
     </section>
   );
 };
